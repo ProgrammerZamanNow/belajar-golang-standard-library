11293decfd1f3860d524791eef16f98738b03741
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-11-07 09:06:29 +0700

    package errors

---
+++ errors.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+var (
+	ValidationError = errors.New("validation error")
+	NotFoundError   = errors.New("not found error")
+)
+
+func GetById(id string) error {
+	if id == "" {
+		return ValidationError
+	}
+
+	if id != "eko" {
+		return NotFoundError
+	}
+
+	// sukses
+
+	return nil
+}
+
+func main() {
+	err := GetById("eko")
+	if err != nil {
+		if errors.Is(err, ValidationError) {
+			fmt.Println("validation error")
+		} else if errors.Is(err, NotFoundError) {
+			fmt.Println("not found error")
+		} else {
+			fmt.Println("unknown error")
+		}
+	}
+}
