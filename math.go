5f15cda8a35ecf4331f411035a59c0417210b2fd
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-11-07 10:18:00 +0700

    math

---
+++ math.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println(math.Ceil(1.40))
+	fmt.Println(math.Floor(1.60))
+	fmt.Println(math.Round(1.60))
+	fmt.Println(math.Max(10, 11))
+	fmt.Println(math.Min(10, 11))
+}
