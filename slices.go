ced3433d452f514afc6cf935786e127420c0dda5
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-11-07 13:36:55 +0700

    slices

---
+++ slices.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+)
+
+func main() {
+	names := []string{"John", "Paul", "George", "Ringo"}
+	values := []int{100, 95, 80, 90}
+
+	fmt.Println(slices.Min(names))
+	fmt.Println(slices.Min(values))
+	fmt.Println(slices.Max(names))
+	fmt.Println(slices.Max(values))
+	fmt.Println(slices.Contains(names, "Eko"))
+	fmt.Println(slices.Index(names, "Eko"))
+	fmt.Println(slices.Index(names, "Paul"))
+}
